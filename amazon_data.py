f09c0b96be772a1151e744b1a1b4844cec78f8fa
Suryavanshi-Gajanan <gajanansuryavanshi1512@gmail.com>
2023-08-25 00:56:25 +0530

    new data file created

--- amazon_data.py
+++ amazon_data.py
@@ -1,12 +1,7 @@
 from autoscraper import AutoScraper
 amazon_url="https://www.amazon.in/s?k=iphones"
-wanted_list=["₹67,999","Apple iPhone 14 (128 GB) - Blue"]
+search_list=["₹67,999","Apple iPhone 14 (128 GB) - Blue"]
 scraper=AutoScraper()
-result=scraper.build(amazon_url,wanted_list)
-# print(result)
-
-print(scraper.get_result_similar(amazon_url,grouped=True))
-
-# scraper.set_rule_aliases({'rule_0sh6':'Title','rule_61o0':'Price'})
-# scraper.keep_rules(['rule_bpu7','rule_61o0'])
-scraper.save('amazon-search')
+result_data=scraper.build(amazon_url,search_list)
+scraper.get_result_similar(amazon_url,grouped=True)
+scraper.save('search-data')
--- search_data.py
+++ search_data.py
@@ -2,37 +2,37 @@ from autoscraper import AutoScraper
 from flask import Flask, request
 
 amazon_scraper = AutoScraper()
-amazon_scraper.load('amazon-search')
+amazon_scraper.load('search-data')
 app = Flask(__name__)
 
 
-def get_amazon_result(search_query):
+def get_amazon_data(search_query):
     url = 'https://www.amazon.in/s?k=%s' % search_query
-    result = amazon_scraper.get_result_similar(url, group_by_alias=True)
-    return _aggregate_result(result)
+    result_data = amazon_scraper.get_result_similar(url, group_by_alias=True)
+    return aggregate_result(result_data)
 
 
-def _aggregate_result(result):
-    final_result = []
+def aggregate_result(result):
+    final_output = []
     print(list(result.values())[0])
     for i in range(len(list(result.values())[0])):
         try:
 
-            final_result.append({alias: result[alias][i] for alias in result})
+            final_output.append({alias: result[alias][i] for alias in result})
         except:
             pass
-    return final_result
+    return final_output
 
 
 @app.route('/', methods=['GET'])
 def search_api():
     query = request.args.get('q')
     print(query)
-    return dict(result=get_amazon_result(query))
+    return dict(result=get_amazon_data(query))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 
