fa424b362cd67dfcf7cc31884d52ddcbf3d009cf
Suryavanshi-Gajanan <gajanansuryavanshi1512@gmail.com>
2023-08-25 01:38:35 +0530

    updated url of amazon in the file

--- amazon_data.py
+++ amazon_data.py
@@ -1,6 +1,6 @@
 from autoscraper import AutoScraper
 amazon_url="https://www.amazon.in/s?k=iphones"
-search_list=["₹67,999","Apple iPhone 14 (128 GB) - Blue"]
+search_list=["₹67,999","Apple iPhone 14 (128 GB) - Purple"]
 scraper=AutoScraper()
 result_data=scraper.build(amazon_url,search_list)
 scraper.get_result_similar(amazon_url,grouped=True)
